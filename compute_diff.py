dba48cdba5ed9c16cc79555b57a6b0433072220b
Aaron Wey <aaronwey@posteo.de>
2025-09-13 16:23:59 +0200

    feat(diff): changes to exported mails

--- compute_diff.py
+++ compute_diff.py
@@ -4,7 +4,8 @@ import utils
 
 
 base_dir = "/home/aaron/tmp_mails/"
-
+all_events_already_done = utils.load_events_from_file()
+import json
 import os
 from pathlib import Path
 
@@ -15,5 +16,13 @@ all_paths = [
     # if os.path.splitext(f)[1] == ".txt"
 ]
 paths_that_jsons = [el for el in all_paths if el.suffix == ".json"]
+events_from_list = []
+for path_el in paths_that_jsons:
+    with open(path_el, "r") as file:
+        try:
+            result = json.load(file)
+        except json.decoder.JSONDecodeError:
+            print(path_el)
+        events_from_list.append(result)
 
-print(all_paths, paths_that_jsons)
+print(all_paths, paths_that_jsons, all_events_already_done)
